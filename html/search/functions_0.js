2afdcc0f7710a25d59cf51feb7c5768742267ed1
lukemartinlogan <lukemartinlogan@users.noreply.github.com>
2022-10-19 01:42:48 +0000

    deploy: 7c9ed216e730720dd542c7df7719180294a62a3d

--- html/search/functions_0.js
+++ html/search/functions_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['_5fopeninitstats_537',['_OpenInitStats',['../classhermes_1_1adapter_1_1mpiio_1_1_mpiio_f_s.html#a61c14fa40946061b9086396f7920c91a',1,'hermes::adapter::mpiio::MpiioFS']]],
+  ['_5fopeninitstats_537',['_OpenInitStats',['../classhermes_1_1adapter_1_1mpiio_1_1_mpiio_f_s.html#a61c14fa40946061b9086396f7920c91a',1,'hermes::adapter::mpiio::MpiioFS::_OpenInitStats()'],['../classhermes_1_1adapter_1_1posix_1_1_posix_f_s.html#afa2761e875008e4cd0cfe55435c890c5',1,'hermes::adapter::posix::PosixFS::_OpenInitStats()'],['../classhermes_1_1adapter_1_1stdio_1_1_stdio_f_s.html#a71045d18f775caa3eb59975e35fa9ff9',1,'hermes::adapter::stdio::StdioFS::_OpenInitStats()']]],
   ['_5frealopen_538',['_RealOpen',['../classhermes_1_1adapter_1_1mpiio_1_1_mpiio_f_s.html#a95e04d8cec1330148bf434a17154c111',1,'hermes::adapter::mpiio::MpiioFS']]]
 ];
