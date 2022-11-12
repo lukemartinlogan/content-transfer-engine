4de7827dc3db313cc231561501009f215c893e81
lukemartinlogan <lukemartinlogan@users.noreply.github.com>
2022-11-12 14:39:46 +0000

    deploy: 04c40033c905ba5ac27552a0e166388689b42df3

--- html/structhermes_1_1adapter_1_1fs_1_1_io_options.js
+++ html/structhermes_1_1adapter_1_1fs_1_1_io_options.js
@@ -4,6 +4,7 @@ var structhermes_1_1adapter_1_1fs_1_1_io_options =
     [ "WithParallelDpe", "structhermes_1_1adapter_1_1fs_1_1_io_options.html#afd81b2c1feb501aea3dda30e536c75e1", null ],
     [ "DirectIo", "structhermes_1_1adapter_1_1fs_1_1_io_options.html#a62307736b8ab0edb2e214e577b3fcc26", null ],
     [ "DataType", "structhermes_1_1adapter_1_1fs_1_1_io_options.html#a351f98a06c6bae8ac668d99cb0f36ca5", null ],
+    [ "PlaceInHermes", "structhermes_1_1adapter_1_1fs_1_1_io_options.html#ad2196ca2e2fcdab111e7fab8104543a4", null ],
     [ "dpe_", "structhermes_1_1adapter_1_1fs_1_1_io_options.html#a91c9908d6e154953ec0998f6b946a396", null ],
     [ "coordinate_", "structhermes_1_1adapter_1_1fs_1_1_io_options.html#a45435830428866a930ce46bc7f5bb00d", null ],
     [ "seek_", "structhermes_1_1adapter_1_1fs_1_1_io_options.html#ac10f6a816abc30aef1f028a0d2d07bbe", null ],
