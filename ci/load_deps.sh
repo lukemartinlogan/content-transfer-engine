bf170cced82d35614321319d05ea517de1941c8a
lukemartinlogan <lukemartinlogan@gmail.com>
2024-11-26 03:08:06 -0600

    Add load_deps.sh

---
+++ ci/load_deps.sh
@@ -0,0 +1,30 @@
+spack load \
+catch2@3.0.1 \
+yaml-cpp \
+doxygen \
+libfabric \
+libelf \
+mochi-thallium+cereal@0.10.1 \
+argobots@1.1+affinity \
+cereal \
+boost@1.7: +context +fiber +coroutine +regex +system +filesystem +serialization +pic +math \
+mpi \
+python \
+py-pip \
+py-pybind11 \
+py-scipy \
+py-numpy \
+py-scikit-learn \
+py-pandas \
+lzo \
+bzip2 \
+zstd \
+lz4 \
+zlib \
+xz \
+brotli \
+snappy \
+c-blosc2
+
+module load hermes_shm
+module load chimaera
