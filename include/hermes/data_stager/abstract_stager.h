87732008c62d502f9bbc2a1e1926de561a706829
lukemartinlogan <lukemartinlogan@gmail.com>
2025-01-09 13:28:44 -0600

    Fix abstractstager virtual destructor

--- include/hermes/data_stager/abstract_stager.h
+++ include/hermes/data_stager/abstract_stager.h
@@ -15,28 +15,20 @@ class AbstractStager {
   std::string params_;
 
   AbstractStager() = default;
-  ~AbstractStager() = default;
+  virtual ~AbstractStager() = default;
 
   virtual void RegisterStager(const hipc::MemContext &mctx,
                               const std::string &tag_name,
                               const std::string &params) = 0;
-  virtual void StageIn(const hipc::MemContext &mctx,
-                       hermes::Client &client,
-                       const TagId &tag_id,
-                       const std::string &blob_name,
+  virtual void StageIn(const hipc::MemContext &mctx, hermes::Client &client,
+                       const TagId &tag_id, const std::string &blob_name,
                        float score) = 0;
-  virtual void StageOut(const hipc::MemContext &mctx,
-                        hermes::Client &client,
-                        const TagId &tag_id,
-                        const std::string &blob_name,
-                        hipc::Pointer &data_p,
-                        size_t data_size) = 0;
-  virtual void UpdateSize(const hipc::MemContext &mctx,
-                          hermes::Client &client,
-                          const TagId &tag_id,
-                          const std::string &blob_name,
-                          size_t blob_off,
-                          size_t data_size) = 0;
+  virtual void StageOut(const hipc::MemContext &mctx, hermes::Client &client,
+                        const TagId &tag_id, const std::string &blob_name,
+                        hipc::Pointer &data_p, size_t data_size) = 0;
+  virtual void UpdateSize(const hipc::MemContext &mctx, hermes::Client &client,
+                          const TagId &tag_id, const std::string &blob_name,
+                          size_t blob_off, size_t data_size) = 0;
 };
 
 }  // namespace hermes
