aacb538e55f2afd794f247e92433c8195bd7f205
Luke Logan <lukemartinlogan@gmail.com>
2025-01-29 02:24:11 -0600

    Test gets past init phase

--- include/hermes/data_stager/stager_factory.h
+++ include/hermes/data_stager/stager_factory.h
@@ -1,46 +1,3 @@
-// //
-// // Created by lukemartinlogan on 9/30/23.
-// //
-
-// #ifndef HERMES_TASKS_DATA_STAGER_SRC_STAGER_FACTORY_H_
-// #define HERMES_TASKS_DATA_STAGER_SRC_STAGER_FACTORY_H_
-
-// #include "abstract_stager.h"
-// #include "binary_stager.h"
-// #include "nvidia_gds_stager.h"
-
-// namespace hermes {
-
-// class StagerFactory {
-//  public:
-//   static std::unique_ptr<AbstractStager> Get(const std::string &path,
-//                                              const std::string &params) {
-//     std::string protocol;
-//     hipc::LocalDeserialize srl(params);
-//     srl >> protocol;
-
-//     std::unique_ptr<AbstractStager> stager;
-//     if (protocol == "file" || protocol == "") {
-//       stager = std::make_unique<BinaryFileStager>();
-//     } else if (protocol == "parquet") {
-//     } else if (protocol == "hdf5") {
-//     } else if (protocol == "nvidia_gds") {
-//       stager = std::make_unique<NvidiaGdsStager>();
-//     } else {
-//       throw std::runtime_error("Unknown stager type");
-//     }
-//     stager->path_ = path;
-//     stager->params_ = params;
-//     return stager;
-//   }
-// };
-
-// }  // namespace hermes
-
-// #endif  // HERMES_TASKS_DATA_STAGER_SRC_STAGER_FACTORY_H_
-
-// Created by lukemartinlogan on 9/30/23.
-//
 
 #ifndef HERMES_TASKS_DATA_STAGER_SRC_STAGER_FACTORY_H_
 #define HERMES_TASKS_DATA_STAGER_SRC_STAGER_FACTORY_H_
@@ -48,7 +5,7 @@
 #include "abstract_stager.h"
 #include "binary_stager.h"
 
-#ifdef HERMES_ENABLE_NVIDIA_GDS_STAGER
+#ifdef HERMES_ENABLE_NVIDIA_GDS_ADAPTER
 #include "nvidia_gds_stager.h"
 #endif
 
@@ -68,7 +25,7 @@ class StagerFactory {
     } else if (protocol == "parquet") {
     } else if (protocol == "hdf5") {
     }
-#ifdef HERMES_ENABLE_NVIDIA_GDS_STAGER
+#ifdef HERMES_ENABLE_NVIDIA_GDS_ADAPTER
     if (protocol == "nvidia_gds") {
       stager = std::make_unique<NvidiaGdsStager>();
     }
